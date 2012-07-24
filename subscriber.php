0cb818ed51743aa946e653935bda2db73ffd893e
Alex Hughes <alexhughes117@gmail.com>
2012-07-24 16:39:57 +0300

    Finished, working version.

--- subscriber.php
+++ subscriber.php
@@ -1,8 +1,19 @@
 <?php
 
+include 'util/emailParser.php';
+include 'util/opendb.php';
+include 'config.php';
+
 $email = $_POST['email'];
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 
+if (validEmail($email)) {
+    insertEmail($subscribeQ, $email, $name, $surname);
+    echo $sucMessage;
+} else {
+    echo $failMessage;
+}
 
+$mysqli->close();
 ?>
---
+++ util/opendb.php
@@ -0,0 +1,16 @@
+<?php
+include './config.php';
+
+/**
+ * Opens a database connection and set the character set to utf-8. 
+ */
+$dbError = false;
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if (mysqli_connect_errno()) {
+    $dbError = true;
+} else {
+    mysqli_set_charset($mysqli, "utf8");
+    $dbError == false;
+}
+?>
