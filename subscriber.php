82d1fa49d6698b3c0006d487f5b5923fc688319b
Alex Hughes <alexhughes117@gmail.com>
2013-08-09 03:00:21 +0100

    Last touches to subscriber, added debugging mode and fromAddress to mail

--- subscriber.php
+++ subscriber.php
@@ -7,13 +7,17 @@ require_once 'util/mail_utils.php';
 require_once 'util/opendb.php';
 require_once 'config.php';
 
+//remove for production version
+//error_reporting(E_ALL | E_STRICT);
+//ini_set("display_errors", "1");
+
 $email = $_POST['email'];
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 
 if (validEmail($email)) {
     if (insertEmail($subscribeQ, $email, $name, $surname)) {
-        sendMail('', $dbName, $emailNotifStart . $email . $emailNotifEnd . $_SERVER['REMOTE_ADDR'] . "</b>", $emailL, 'subscriber');
+        sendMail($fromAddress, $dbName, $emailNotifStart . $email . $emailNotifEnd . $_SERVER['REMOTE_ADDR'] . "</b>", $emailL, 'subscriber');
         echo $sucMessage;
     } else {
         echo $failMessage;
